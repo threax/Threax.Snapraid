a7c638e1c29538d265af6662dac352023351f67e
Andrew Piper <andrew@threax.com>
2021-11-24 10:51:19 -0500

    Add push script.

---
+++ Push.ps1
@@ -0,0 +1,4 @@
+git push
+git push https://github.com/threax/Threax.Snapraid.git
+git push https://gitlab.com/threax-software/Threax.Snapraid.git
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.Snapraid
